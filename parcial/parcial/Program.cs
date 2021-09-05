3e433aab580bda256817f6fffc2902d137ae711c
WilberJose <wilberjose200208@gmail.com>
2021-09-05 15:08:57 -0600

    Crear archivos, comentarios sobre la materia

--- parcial/parcial/Program.cs
+++ parcial/parcial/Program.cs
@@ -12,24 +12,52 @@ namespace parcial
     {
         static void main(string[] args)
         {
-     
 
 
 
-        string path = (@"C:\PARCIAL1\Libreria\registro.text");
+
+            string path = (@"C:\PARCIAL1\Libreria\registro.text");
 
 
             using (StreamWriter archivo = File.AppendText(path))
             {
-             
+
+                archivo.WriteLine("REGISTRO E INICIO DE SESION");
+
+
+
+                string archivo1 = (@"C:\PARCIAL1\Libreria\Merary.text");
+
+                using (StreamWriter Merary = File.AppendText(archivo1))
+                {
+
+                    Merary.WriteLine("");
+                }
+                string archivo2 = (@"C:\PARCIAL1\Libreria\Audali.text");
+
+                using (StreamWriter Audali = File.AppendText(archivo2))
+                {
+                    Audali.WriteLine("");
+
+                }
+                string archivo3 = (@"C:\PARCIAL1\Libreria\Wilber.text");
+
+                using (StreamWriter Wilber = File.AppendText(archivo3))
+                {
+                    Wilber.WriteLine("Siento que una de las mayores dificultades en este parcial fue la creación de códigos y que estos funcionen correctamente ya que al no funcionar seria de crearlos nuevamente haciendo así el consumo de bastante tiempo realizando los nuevos códigos para que estos funcionen por eso siento que la creación de códigos fue lo mas complicado");
+                }
+                string archivo4 = (@"C:\PARCIAL1\Libreria\Luis.text");
+
+                using (StreamWriter Luis = File.AppendText(archivo4))
+                {
+                    Luis.WriteLine("");
+                }
+
+            }
+
+
+            Console.ReadKey();
         }
-          
 
-                   Console.ReadKey();
-        }
-
+    }
 }
-}
-
-
-
